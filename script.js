403d5925a4d3637e214c5cc66fb2ea74a2842fee
Richard Li <79170351+RichardLi-1@users.noreply.github.com>
2024-09-27 23:36:58 -0400

    true

---
+++ script.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+
+function showSlides(n) {
+    let slides = document.querySelectorAll('.slide');
+    let slidesWrapper = document.querySelector('.slides-wrapper');
+    let numSlides = slides.length;
+
+    slideIndex = (n + numSlides) % numSlides;
+
+    slidesWrapper.style.transform = 'translateX(' + (-slideIndex * 100 / numSlides) + '%)';
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex + n);
+}
+
+// Initialize the first set of slides
+showSlides(slideIndex);
+
+function toggleMenu(){
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('show');
+  }
